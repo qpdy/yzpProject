2aa6f05eae7c538467eaefb739452daf2495deca
yangzhanpeng <446610985@qq.com>
2026-03-15 15:46:47 +0800

    chore(config): 更新站点基础路径和项目名称

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://qpdy.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/frontend-docs/',
+  baseUrl: '/yzpProject/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'qpdy', // Usually your GitHub org/user name.
-  projectName: 'frontend-docs', // Usually your repo name.
+  projectName: 'yzpProject', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
